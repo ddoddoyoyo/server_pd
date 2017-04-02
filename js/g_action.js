13846222d39efea748335bf7fb39589252e29ace
hye yeon park <purely36@gmail.com>
2017-04-03 03:09:25 +0900

    손구락 다시 가리기

--- js/g_action.js
+++ js/g_action.js
@@ -2040,7 +2040,7 @@ $(document).ready(function(){
 		    })
 		    .fadeIn(500);
 		    $("#page85 #textbox2, #page85 .next_p_btn").delay(800).fadeIn();
-			$(this).delay(2000).fadeIn();
+			//$(this).delay(2000).fadeIn();
 		} else if(next_Count == 2){
 			$.mobile.changePage("#page86");
 		}
@@ -2071,7 +2071,7 @@ $(document).ready(function(){
 		    })
 		    .fadeIn(500);
 		    $("#page87 #textbox2, #page87 .next_p_btn").delay(800).fadeIn();
-			$(this).delay(2000).fadeIn();
+			//$(this).delay(2000).fadeIn();
 		} else if(next_Count == 2){
 			$.mobile.changePage("#page88");
 		}
