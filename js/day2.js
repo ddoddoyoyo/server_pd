134150572c3b8002d2c0457281dcf9989fb837c6
hye yeon park <purely36@gmail.com>
2017-04-05 15:07:27 +0900

    90 뿅~!

--- js/day2.js
+++ js/day2.js
@@ -1419,6 +1419,18 @@ $(document).ready(function(){
 		}
 	});
 
+	$("#page90").on({
+		"pagebeforeshow" : function(){
+			$("#page90 .textwrap, #page83 .next_p_btn").hide();
+			$("#page90").css({"background-size":"120% auto"});
+		}, 
+		"pageshow" : function(){
+			$("#page90").animate({"backgroundSize":"100%"});
+			$("#page90 .textwrap").delay(500).fadeIn(500);
+			$("#page90 .next_p_btn").delay(1000).fadeIn(500);
+		}
+	});
+
 	$("#page90 .next_p_btn").click(function(){
 		$("#page90 .audio1").each(function(){ 
 		this.pause();
