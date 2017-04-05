d49a986180b6a2ef44bf0a241bbc156e4f16bf3e
hye yeon park <purely36@gmail.com>
2017-04-05 18:19:15 +0900

    menu TT

--- en/index.php
+++ en/index.php
@@ -49,22 +49,17 @@
 						reader.onload = function (e) {
 						//파일 읽어들이기를 성공했을때 호출되는 이벤트 핸들러
 							$("#current-img").attr("src", e.target.result);
-							// image.onload=function(){
-							// 	var w =this.width;
-							// 	var h= this.height;
-							// 	if(h >= w){
-							// 		$("#current-img").css("width":"100%","height":"auto"});
-							// 	} else {
-							// 		$("#current-img").css({"width":"auto", "height":"100%"});
-							// 	}
-							// }
-							console.log("H: " + $("#current-img").height());
-							console.log("W: " + $("#current-img").width());
-							if($("#current-img").height() >= $("#current-img").width()){
-								$("#current-img").css({"width":"100%","height":"auto"});
-							} else {
-								$("#current-img").css({"width":"auto", "height":"100%"});
-							} 
+							 image.onload=function(){
+							 	var w =this.width;
+							 	var h= this.height;
+							 	if(h >= w){
+							 		$("#current-img").css({"width":"100%","height":"auto"});
+							 	} else {
+							 		$("#current-img").css({"width":"auto", "height":"100%"});
+							 	}
+							 	console.log("H: " + $("#current-img").height());
+								console.log("W: " + $("#current-img").width());
+								 }
 						} 
 							reader.readAsDataURL(input.files[0]);                 
 						
